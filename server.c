cb25fad0b4a6971186ffd03d27c9481e5e69ff05
033 <sp15sp@yandex.ru>
2020-02-05 09:01:40 +0700

    SigintHandler

--- server.c
+++ server.c
@@ -7,22 +7,22 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
-int cycle = 1;
+int sock[2];
 void SigintHandler(int sig)
 {
-    cycle = 0;
     printf("\nServer out\n");
+    close(sock[0]);
     exit(0);
 }
 
-int err_handler(int func, const char *errstr, int quantity, int *sock)
+int err_handler(int func, const char *errstr, int quantity, int first)
 {
     if(func < 0)
     {
         if(quantity > 0)
         {
             int i;
-            for(i = 0; i < quantity; i++)
+            for(i = first; i < quantity; i++)
                 close(sock[i]);
         }
         perror(errstr);
@@ -47,7 +47,7 @@ int main()
 
     int buf;
     char buf2[16] = "ready\n";
-    int sock[2], bytes_read;
+    int bytes_read;
     struct sockaddr_in addr, child, parent;
     socklen_t size_p = sizeof(parent), size_c = sizeof(child);
     pid_t child_pid;
@@ -56,29 +56,27 @@ int main()
     addr.sin_family = AF_INET;
     addr.sin_port = htons(0);
     addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-    err_handler(bind(sock[0], (struct sockaddr*)&addr, sizeof(addr)), "bind", 1, sock);
-    err_handler(getsockname(sock[0], (struct sockaddr*)&parent, &size_p), "getsockname", 1, sock);
+    err_handler(bind(sock[0], (struct sockaddr*)&addr, sizeof(addr)), "bind", 1, 0);
+    err_handler(getsockname(sock[0], (struct sockaddr*)&parent, &size_p), "getsockname", 1, 0);
     printf("Port: %d\n", ntohs(parent.sin_port));
     listen(sock[0], 6);
-    while(cycle)
+    while(1)
     {
-        sock[1] = err_handler(accept(sock[0], (struct sockaddr*)&child, &size_c), "accept", 1, sock);
-        child_pid = err_handler(fork(), "fork", 2, sock);
+        sock[1] = err_handler(accept(sock[0], (struct sockaddr*)&child, &size_c), "accept", 1, 0);
+        child_pid = err_handler(fork(), "fork", 2, 0);
         if(child_pid == 0)
         {
         	close(sock[0]);
             int i, n;
-            err_handler(send(sock[1], buf2, sizeof(buf2), 0), "send", 1, &sock[1]);
-            bytes_read = err_handler(recv(sock[1], &n, sizeof(int), 0), "recv", 1, &sock[1]);
+            err_handler(send(sock[1], buf2, sizeof(buf2), 0), "send", 1, 1);
+            bytes_read = err_handler(recv(sock[1], &n, sizeof(int), 0), "recv", 1, 1);
         	for(i = 0; i < n; i++)
         	{
-    	        bytes_read = err_handler(recv(sock[1], &buf, sizeof(int), 0), "recv", 1, &sock[1]);
+    	        bytes_read = err_handler(recv(sock[1], &buf, sizeof(int), 0), "recv", 1, 1);
                 printf("%d\n", buf);
             }
             exit(0);
         }
         close(sock[1]);
     }
-    close(sock[0]);
-    exit(0);
 }
